14275cbec29461df872403bd54668c2d73db0484
Ralph Giles <giles@thaumas.net>
2019-05-08 15:22:20 -0700

    Repace delegated with Z for linux.
    The docker volume bind-mount option 'delegated' is specific
    to macOS. The documentation says it's ignored on other platforms,
    but on my machine (Fedora 30 x86_64) it throws an error.
    
    I've removed it, but making the script portable with some kind
    of os detection would work too.
    
    Similarly, selinux requires the Z attribute on my system
    to allow the bind mount into the container.

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ BASE="$( cd "$(dirname "$0")" ; pwd -P )"
 echo "Building in ${BASE}"
 
 docker run \
--v "${BASE}/src:delegated" \
--v "${BASE}/zips:/srv/zips:delegated" \
--v "${BASE}/logs:/srv/logs:delegated" \
--v "${BASE}/ccache:/srv/ccache:delegated" \
+-v "${BASE}/src:/srv/src:Z" \
+-v "${BASE}/zips:/srv/zips:Z" \
+-v "${BASE}/logs:/srv/logs:Z" \
+-v "${BASE}/ccache:/srv/ccache:Z" \
 -e "BRANCH_NAME=v1-nougat" \
 -e "DEVICE_LIST=hammerhead" \
 -e "CUSTOM_PACKAGES='MuPDF GmsCore GsfProxy FakeStore com.google.android.maps.jar Telegram Signal Mail BlissLauncher BlissIconPack MozillaNlpBackend OpenWeatherMapWeatherProvider AccountManager MagicEarth OpenCamera eDrive Weather Notes Tasks NominatimNlpBackend Light DroidGuard OpenKeychain QKSMS Bromite BromiteWebView LibreOfficeViewer'" \
